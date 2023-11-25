8daef1f893ab4cbb8879102be1b1eff8157e6595
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-25 16:52:29 -0500

    Adds content to navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,11 @@
+import { Logo } from "./Logo";
+
 export const NavBar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className="md:max-w--screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+      </div>
+    </div>
+  );
 };
