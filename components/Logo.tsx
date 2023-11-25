d26ba94e5ad7a2e9ee59a13aab89afde03543799
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-25 16:40:54 -0500

    Adds custom font to logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/font.woff2",
+});
+
 export const Logo = () => {
   return (
     <Link href={"/"}>
       <div className="hover:opacity-75 transition hidden md:flex items-center gap-x-2">
         <Image src={"/logo.svg"} alt="Logo" height={30} width={30} />
-        <p className="text-lg text-neutral-700 pb-1">FlowBoard</p>
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          FlowBoard
+        </p>
       </div>
     </Link>
   );
