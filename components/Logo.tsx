1e5a30f48fb29e447dca640dc239b6fd2e399130
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-25 16:55:00 -0500

    Fixes heading font for logo module not found error

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,17 +6,15 @@ import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
 const headingFont = localFont({
-  src: "../public/font.woff2",
+  src: "../public/fonts/font.woff2",
 });
 
 export const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="hover:opacity-75 transition hidden md:flex items-center gap-x-2">
+      <div className="hover:opacity-75 transition hidden md:flex items-center justify-center gap-x-2">
         <Image src={"/logo.svg"} alt="Logo" height={30} width={30} />
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           FlowBoard
         </p>
       </div>
