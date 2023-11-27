f1803a17696138c4a65c34b3947802eef268a319
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-27 16:23:24 -0500

    Adds organization select to manage different boards

---
+++ app/(content)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,13 @@
+import { OrganizationList } from "@clerk/clerk-react";
+
+const CreateOrganizationPage = () => {
+  return (
+    <OrganizationList
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
+};
+
+export default CreateOrganizationPage;
