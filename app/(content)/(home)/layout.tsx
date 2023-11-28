ba3f06177a0fd1f5ba26481639c113dcc1ed81c4
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-28 15:06:23 -0500

    Fixes undefined component issue

--- app/(content)/(home)/layout.tsx
+++ app/(content)/(home)/layout.tsx
@@ -1,9 +1,9 @@
-import { NavBar } from "@/components/Navbar";
+import UserNavBar from "@/components/layout/UserNavBar";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <NavBar />
+      <UserNavBar />
       {children}
     </div>
   );
