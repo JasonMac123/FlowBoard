55aef4f577ff380c996ab9cf883bdafccc6eb353
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-28 15:10:12 -0500

    Adds styling for user navbar when user is logged in

--- components/layout/UserNavBar.tsx
+++ components/layout/UserNavBar.tsx
@@ -1,5 +1,15 @@
+import { Logo } from "./Logo";
+
 const UserNavBar = () => {
-  return <div></div>;
+  return (
+    <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserNavBar;
