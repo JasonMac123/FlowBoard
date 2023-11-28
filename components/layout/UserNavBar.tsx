de61d2a05cc37bc5dbf47b041ccc1483613cb6af
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-28 15:11:23 -0500

    Uses semantic html tags instead of divs

--- components/layout/UserNavBar.tsx
+++ components/layout/UserNavBar.tsx
@@ -2,13 +2,13 @@ import { Logo } from "./Logo";
 
 const UserNavBar = () => {
   return (
-    <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 top-0 w-full px-4 h-14 border-b shadow-sm bg-white flex items-center">
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
