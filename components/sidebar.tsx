da9f528cf767a6de56be8c48e6d37c99e1757af3
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-29 16:27:08 -0500

    Adds onloading sidebar animation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,61 @@
 "use client";
 
-export const SideBar = () => {
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import {
+  currentUser,
+  useOrganization,
+  useOrganizationList,
+} from "@clerk/nextjs";
+
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { Skeleton } from "./ui/skeleton";
+import { Accordion } from "./ui/accordion";
+
+interface SideBarProps {
+  storageKey?: string;
+}
+
+export const SideBar: React.FC<SideBarProps> = ({
+  storageKey = "t-sidebar-state",
+}) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOranization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: { infinite: true },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
   return <div></div>;
 };
