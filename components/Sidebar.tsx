e7035b23d2dd40e8089925d55bac288df4ba192b
JasonSnow123 <m.jason.2018@gmail.com>
2023-11-30 15:38:10 -0500

    Adds a loading skeleton for mobile side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -55,7 +55,10 @@ export const SideBar: React.FC<SideBarProps> = ({
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
       </>
     );
   }
