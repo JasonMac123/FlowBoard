f8f62316a106f4992a8f0508e26ecb8bbf349bd9
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-01 15:30:13 -0500

    Adds settings information about organization from clerk js

--- app/(content)/(home)/organization/[organizationId]/settings/page.tsx
+++ app/(content)/(home)/organization/[organizationId]/settings/page.tsx
@@ -1,5 +1,11 @@
+import { OrganizationProfile } from "@clerk/nextjs";
+
 const OrganizationSettingsPage = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <OrganizationProfile />
+    </div>
+  );
 };
 
 export default OrganizationSettingsPage;
