096c1fc484b06e35eb64e24b3711f1e620720f0e
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-04 17:06:32 -0500

    Adds board list to organization

--- app/(content)/(home)/organization/[organizationId]/page.tsx
+++ app/(content)/(home)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
+import { BoardList } from "@/components/organization/BoardList";
 import { Info } from "@/components/organization/Info";
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
