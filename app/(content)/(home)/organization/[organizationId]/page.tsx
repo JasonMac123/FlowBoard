7de79f5fc366f4895071468945e453d10d818449
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-03 21:06:38 -0500

    Creates form component for boards

--- app/(content)/(home)/organization/[organizationId]/page.tsx
+++ app/(content)/(home)/organization/[organizationId]/page.tsx
@@ -1,30 +1,9 @@
 import { db } from "@/prisma/db";
 
-import { Button } from "@/components/ui/button";
-import { create } from "@/app/functions/create-board";
-
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
 
-  return (
-    <div>
-      <form action={create}>
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a board title"
-          className="bborder-black border p-1"
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <div key={board.id}>{board.title}</div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default OrganizationIdPage;
---
+++ components/Form.tsx
@@ -0,0 +1,24 @@
+"use server";
+
+import { createBoard } from "@/functions/create-board";
+
+import { useAction } from "@/hooks/useAction";
+
+export const Form = () => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, "success");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
+  return <form action=""></form>;
+};
