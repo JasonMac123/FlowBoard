9b1565fa3362541d71cc87e2bab59041468e573c
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-04 15:25:39 -0500

    Adds info loading skeleton

--- components/organization/Info.tsx
+++ components/organization/Info.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
+import { Skeleton } from "../ui/skeleton";
 
 interface InfoProps {}
 
@@ -9,7 +10,7 @@ export const Info = () => {
   const { organization, isLoaded } = useOrganization();
 
   if (!isLoaded) {
-    return <p>Loading!</p>;
+    return <Info.Skeleton />;
   }
 
   return (
@@ -21,6 +22,22 @@ export const Info = () => {
           alt="organization photo"
           className="rounded-md object-cover"
         />
+      </div>
+      <div className="space-y-1">
+        <p className="font-semibold text-xl">{organization?.name}</p>
+      </div>
+    </div>
+  );
+};
+
+Info.Skeleton = function SkeletonInfo() {
+  return (
+    <div className="flex items-center gap-x-4">
+      <div className="w-[60px] h-[60px] relative">
+        <Skeleton className="w-full h-full absolute" />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-10 w-[200px]" />
       </div>
     </div>
   );
