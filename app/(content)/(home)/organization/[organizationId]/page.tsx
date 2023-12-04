b16fd8a1b0a137666c1c51cdd46c2c83fe9d497c
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-03 20:26:43 -0500

    Adds ability to craete boards

--- app/(content)/(home)/organization/[organizationId]/page.tsx
+++ app/(content)/(home)/organization/[organizationId]/page.tsx
@@ -1,5 +1,30 @@
-const OrganizationIdPage = () => {
-  return <div></div>;
+import { db } from "@/prisma/db";
+
+import { Button } from "@/components/ui/button";
+import { create } from "@/app/functions/create-board";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="bborder-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
---
+++ app/functions/create-board.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { db } from "@/prisma/db";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+  title: z.string(),
+});
+
+export async function create(formData: FormData) {
+  const { title } = CreateBoard.parse({
+    title: formData.get("title"),
+  });
+
+  await db.board.create({
+    data: {
+      title,
+    },
+  });
+}
