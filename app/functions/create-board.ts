e1a838bab9e6e937eb5d2f03e9baabb40e523cde
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-03 20:31:28 -0500

    Adds minimum length of board title when creating board

--- app/functions/create-board.ts
+++ app/functions/create-board.ts
@@ -4,7 +4,9 @@ import { db } from "@/prisma/db";
 import { z } from "zod";
 
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: "Minimum length of 3 letters is required",
+  }),
 });
 
 export async function create(formData: FormData) {
