840d4c37c17748ee67370da8a81c344e0f79b5e7
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-05 15:37:07 -0500

    Adds board creation form in form pop-over

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/popover";
+import { Button } from "../ui/button";
 import { useAction } from "@/hooks/useAction";
 import { createBoard } from "@/functions/create-board";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { X } from "lucide-react";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -32,6 +39,20 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-700"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form className="space-y-4">
+          <div className="space-y-4">
+            <FormInput id="title" label="Board title" type="text" />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
       </PopoverContent>
     </Popover>
   );
