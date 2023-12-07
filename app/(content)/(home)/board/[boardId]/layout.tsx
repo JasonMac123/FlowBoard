2f9be2ca1df6c05c4a1b78f2ec6455d2d74a2dca
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-07 16:19:03 -0500

    Fetchs board data using prisma and redirects to 404 not found if failed

--- app/(content)/(home)/board/[boardId]/layout.tsx
+++ app/(content)/(home)/board/[boardId]/layout.tsx
@@ -1,10 +1,31 @@
-const BoardIdLayout = ({
+import { db } from "@/prisma/db";
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+
+const BoardIdLayout = async ({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { boardId: string };
 }) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
   return (
     <div>
       <main className="relative pt-28 h-full">{children}</main>
