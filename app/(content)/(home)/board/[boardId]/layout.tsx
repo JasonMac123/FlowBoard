15f696c65ba89b1edc0d3077e4642e9ddbf7fdf4
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-08 16:05:49 -0500

    Passes down board title and renders it in a button

--- app/(content)/(home)/board/[boardId]/layout.tsx
+++ app/(content)/(home)/board/[boardId]/layout.tsx
@@ -58,7 +58,8 @@ const BoardIdLayout = async ({
       className="relative h-full bg-no-repeat bg-cover bg-center"
       style={{ backgroundImage: `url(${board.imageFullUrl})` }}
     >
-      <BoardNavBar />
+      <BoardNavBar data={board} />
+      <div className="absolute inset-0 bg-black/10" />
       <main className="relative pt-28 h-full">{children}</main>
     </div>
   );
