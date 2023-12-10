0a37393305dc51cf77441c6ce70727ac8bf97157
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-09 20:09:33 -0500

    Adds form input for editing board title in navbar

--- components/form/board-title-form.tsx
+++ components/form/board-title-form.tsx
@@ -1,15 +1,46 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
 import { Board } from "@prisma/client";
 import { Button } from "../ui/button";
+import { FormInput } from "./form-input";
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {};
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <form ref={formRef} className="flex itesm-center gap-x-2">
+        <FormInput
+          ref={inputRef}
+          id="title"
+          onBlur={() => {}}
+          defaultValue={data.title}
+          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+        />
+      </form>
+    );
+  }
+
   return (
-    <Button className="font-bold text-lg h-auto w-auto p-1 px-2">
+    <Button
+      onClick={enableEditing}
+      className="font-bold text-lg h-auto w-auto p-1 px-2"
+      variant="transparent"
+    >
       {data.title}
     </Button>
   );
