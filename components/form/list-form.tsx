900e2b04dc67a590ae21eb4c9d6f59481995accc
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-11 17:58:29 -0500

    Adds onclick function for button to allow user to edit title

--- components/form/list-form.tsx
+++ components/form/list-form.tsx
@@ -5,6 +5,7 @@ import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { useState, useRef, ElementRef } from "react";
 
 import { ListWrapper } from "../board/ListWrapper";
+import { FormInput } from "./form-input";
 
 export const ListForm = () => {
   const [isEditing, setIsEditing] = useState(false);
@@ -32,9 +33,29 @@ export const ListForm = () => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            id="title"
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+          />
+        </form>
+      </ListWrapper>
+    );
+  }
+
   return (
     <ListWrapper>
-      <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
+      <button
+        onClick={enableEditing}
+        className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
+      >
         <Plus className="h-4 w-4 mr-2" />
         Add a list
       </button>
