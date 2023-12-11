3ae0f4a98f8ed58b383f8ec88c6e60827ae5bd9e
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-11 14:51:48 -0500

    Adds board function to delete into board options

--- components/layout/BoardOptions.tsx
+++ components/layout/BoardOptions.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { MoreHorizontal, X } from "lucide-react";
+import toast from "react-hot-toast";
+
+import { deleteBoard } from "@/functions/delete-board";
+import { useAction } from "@/hooks/useAction";
 
 import { Button } from "../ui/button";
 import {
@@ -15,6 +19,16 @@ interface BoardOptionsProps {
 }
 
 export const BoardOptions = ({ id }: BoardOptionsProps) => {
+  const { execute, isLoading } = useAction(deleteBoard, {
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = () => {
+    execute({ id });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -36,7 +50,8 @@ export const BoardOptions = ({ id }: BoardOptionsProps) => {
         </PopoverClose>
         <Button
           variant="ghost"
-          onClick={() => {}}
+          onClick={onDelete}
+          disabled={isLoading}
           className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
         >
           Delete this board
