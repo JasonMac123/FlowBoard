54c2328558ece35f3e6c67378a7fae165f51a732
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-11 17:54:22 -0500

    Adds form with escape functions for esc and clicking outside of the form

--- components/form/list-form.tsx
+++ components/form/list-form.tsx
@@ -1,5 +1,43 @@
 "use client";
 
+import { Plus } from "lucide-react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { useState, useRef, ElementRef } from "react";
+
+import { ListWrapper } from "../board/ListWrapper";
+
 export const ListForm = () => {
-  return <div></div>;
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  return (
+    <ListWrapper>
+      <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
+        <Plus className="h-4 w-4 mr-2" />
+        Add a list
+      </button>
+    </ListWrapper>
+  );
 };
