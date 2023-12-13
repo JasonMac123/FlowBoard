7aa295741bfc83f49ff8798a176c0b9521842c77
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-13 15:05:43 -0500

    Adds input for editing title list

--- components/board/ListHeader.tsx
+++ components/board/ListHeader.tsx
@@ -1,17 +1,65 @@
 "use client";
 
+import { useState, useRef, ElementRef } from "react";
+import { useEventListener } from "usehooks-ts";
+
 import { List } from "@prisma/client";
+
+import { FormInput } from "../form/form-input";
 
 interface ListHeaderProps {
   data: List;
 }
 
 export const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        {data.title}
-      </div>
+      {isEditing ? (
+        <form action="" className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            ref={inputRef}
+            onBlur={() => {}}
+            id="title"
+            placeholder="Enter list title"
+            defaultValue={title}
+          />
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {data.title}
+        </div>
+      )}
     </div>
   );
 };
