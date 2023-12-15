b1055baa1f84dd237bee6c2e95e75056cd75c6ac
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-15 17:52:19 -0500

    Styles text area component for forms

--- components/form/form-textarea.tsx
+++ components/form/form-textarea.tsx
@@ -2,6 +2,11 @@
 
 import { KeyboardEventHandler, forwardRef } from "react";
 
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+import { cn } from "@/lib/utils";
+import { FormErrors } from "./form-errors";
+
 interface FormTextAreaProps {
   id: string;
   label?: string;
@@ -17,20 +22,54 @@ interface FormTextAreaProps {
 }
 
 export const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>(
-  ({
-    id,
-    label,
-    placeholder,
-    required,
-    disabled,
-    errors,
-    onBlur,
-    onClick,
-    onKeyDown,
-    classname,
-    defaultValue,
-  }) => {
-    return <div></div>;
+  (
+    {
+      id,
+      label,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      onBlur,
+      onClick,
+      onKeyDown,
+      classname,
+      defaultValue,
+    },
+    ref
+  ) => {
+    return (
+      <div className="space-y-2 w-full">
+        <div className="space-y-1 w-full">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Textarea
+            onKeyDown={onKeyDown}
+            onBlur={onBlur}
+            ref={ref}
+            onClick={onClick}
+            required={required}
+            placeholder={placeholder}
+            name={id}
+            id={id}
+            disabled={disabled}
+            className={cn(
+              "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 focus:ring-0 outline-none shadow-sm",
+              classname
+            )}
+            aria-describedby={`${id}-error`}
+            defaultValue={defaultValue}
+          />
+        </div>
+        <FormErrors id={id} errors={errors} />
+      </div>
+    );
   }
 );
 
