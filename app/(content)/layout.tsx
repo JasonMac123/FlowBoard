920d18720eb1109c59e82bf1fa68e08ae91c8147
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-19 15:03:04 -0500

    Adds card modal to layout

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,11 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs";
-
 import { Toaster } from "react-hot-toast";
+
+import { ModalProvider } from "@/components/modals/ModalProvider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <Toaster />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   );
