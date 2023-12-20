fe5f5428c893b29198b98e2092440aae9240ccf2
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-20 18:18:37 -0500

    Changes schema to make title optional to accept changing description of card modal

--- functions/update-card/schema.ts
+++ functions/update-card/schema.ts
@@ -1,23 +1,23 @@
 import { z } from "zod";
 
 export const UpdateCard = z.object({
-  title: z
-    .string({
+  title: z.optional(
+    z.string({
       required_error: "Title is required",
       invalid_type_error: "Title is required",
     })
     .min(3, {
       message: "Title is too short",
-    }),
+    })
+  ),
   description: z.optional(
-    z
-      .string({
-        required_error: "Description is required",
-        invalid_type_error: "Description is required",
-      })
-      .min(3, {
-        message: "Description is too short.",
-      })
+    z.string({
+      required_error: "Description is required",
+      invalid_type_error: "Description is required",
+    })
+    .min(3, {
+      message: "Description is too short.",
+    })
   ),
   id: z.string(),
   boardId: z.string(),
