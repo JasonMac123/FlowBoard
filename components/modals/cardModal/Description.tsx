df8b2f26f4028cf61f671818e83931f43aae4b42
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-20 15:23:38 -0500

    Creates descritption skeleton for loading purposes

--- components/modals/cardModal/Description.tsx
+++ components/modals/cardModal/Description.tsx
@@ -2,10 +2,24 @@
 
 import { CardWithList } from "@/types";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface DescriptionProps {
   data: CardWithList;
 }
 
 export const Description = ({ data }: DescriptionProps) => {
   return <div>{data.description}</div>;
+};
+
+Description.skeleton = function DescriptionSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <Skeleton className="h-6 w-6 bg-neutral-200" />
+      <div className="w-full">
+        <Skeleton className="h-6 w-24 mb-2 bg-neutral-200" />
+        <Skeleton className="h-6 w-24 mb-2 bg-neutral-200" />
+      </div>
+    </div>
+  );
 };
