7a23797522535fbf429c273e5f0a42aa5e88e297
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-21 16:02:07 -0500

    Creates action skeleton to show when data is still loading

--- components/modals/cardModal/Actions.tsx
+++ components/modals/cardModal/Actions.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Actions = () => {
   return (
     <div>
       <p>Actions</p>
+    </div>
+  );
+};
+
+Actions.skeleton = function ActionSkeleton() {
+  return (
+    <div className="space-y-2 mt-2">
+      <Skeleton className="w-20 h-4 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
+      <Skeleton className="w-full h-8 bg-neutral-200" />
     </div>
   );
 };
