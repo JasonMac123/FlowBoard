a20b223082e02d500eb1ba29a93b9229227cfbd5
JasonSnow123 <m.jason.2018@gmail.com>
2023-12-26 14:44:37 -0500

    Creates activity for card modal to show updates

---
+++ components/modals/cardModal/Activity.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const Activity = () => {
+  return <div></div>;
+};
+
+Activity.Skeleton = function ActivitySkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <Skeleton className="h-6 w-6 bg-neutral-200" />
+      <div className="w-full">
+        <Skeleton className="w-24 h-6 mb-2 bg-neutral-200" />
+        <Skeleton className="w-full h-10 bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
