51c6a87b1fdde7d97bad065fa2d32d29e34fbe33
JasonSnow123 <m.jason.2018@gmail.com>
2024-01-01 22:18:22 -0500

    Shows activity item for all audit log items

--- components/organization/ActivityList.tsx
+++ components/organization/ActivityList.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/prisma/db";
 import { ActivityItem } from "../modals/cardModal/ActivityItem";
 import { Skeleton } from "../ui/skeleton";
+import { log } from "console";
 
 export const ActivityList = async () => {
   const { orgId } = auth();
@@ -11,4 +12,21 @@ export const ActivityList = async () => {
   if (!orgId) {
     redirect("/select-org");
   }
+
+  const auditLogs = await db.auditLog.findMany({
+    where: {
+      orgId,
+    },
+  });
+
+  return (
+    <ol className="space-y-4 mt-4">
+      <p className="hidden last:block text-xs text-cetner text-muted-foreground">
+        No Activity found within this organization
+      </p>
+      {auditLogs.map((log) => (
+        <ActivityItem key={log.id} data={log} />
+      ))}
+    </ol>
+  );
 };
